fe4f5050eb172bf854a0f1abdb5da01d4f65622b
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2026-08-12 10:16:23 +0000

    Add migration files to remove unique constraints and update registration models and serializers

--- cstep_backend/registrations/serializers.py
+++ cstep_backend/registrations/serializers.py
@@ -253,13 +253,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
             "updated_at",
         ]
 
-        # validators = [
-        #     UniqueTogetherValidator(
-        #         queryset=Registration.objects.all(),
-        #         fields=["user", "event"],
-        #         message="You have already registered for this event."
-        #     )
-        # ]
 
     def validate(self, attrs):
         event = attrs["event"]
@@ -334,6 +327,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         return registration
 
     def update(self, instance, validated_data):
+        validated_data.pop("user", None)
         session_data = validated_data.pop("sessions", None)
 
         for attr, value in validated_data.items():
